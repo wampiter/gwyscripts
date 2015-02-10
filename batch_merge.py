e5df5e07d5bc65829616282b1b48c4f35249f97f
Scott R. Johnston <wampiter@gmail.com>
2015-02-10 14:06:10 -0800

    working batch_merge

--- batch_merge.py
+++ batch_merge.py
@@ -3,26 +3,23 @@ from glob import glob
 filebase = "/Users/jot/Desktop/test"
 channels = ['Topo', 'AUX1', 'AUX2', 'Amplitude0', 'Phase0', 'Amplitude 1st', 'Phase 1st']
 
-containers = {}
-for channel in channels:
+#Load all from channels by channel and scan number:
+containers = []
+for chan_index, channel in enumerate(channels):
 	paths = glob(filebase + "/*" + channel + "*.tiff")
 	for path in paths:
 		number = path[-8:-5]
+		while len(containers) <= int(number):
+			containers.append([None]*len(channels))
 		c = gwy_file_load(path, gwy.RUN_NONINTERACTIVE)
-		containers[channel, number] = c
+		containers[int(number)][chan_index] = c
 
-scannumbers = [path[-8:-5] for path in topopaths]
-
-
-for path in aux1paths:
-	c = gwy_file_load(path, gwy.RUN_NONINTERACTIVE)
-	aux1cons[path[-8:-5]] = c
-
-c1 = gwy_file_load("%s/test.tiff" % filebase, gwy.RUN_NONINTERACTIVE)
-c2 = gwy_file_load("%s/test2.tiff" % filebase, gwy.RUN_NONINTERACTIVE)
-
-gwy.gwy_app_data_browser_add(c1)
-gwy_app_data_browser_select_data_field(c1, 0)
-gwy_app_data_browser_merge(c2)
-
-gwy_file_save(c1, "testy.gwy", gwy.RUN_NONINTERACTIVE)
+#Merge other channels into topography container, save:
+for number, topo_container in enumerate([scan[0] for scan in containers]):
+	if topo_container != None:
+		gwy_app_data_browser_add(topo_container)
+		gwy_app_data_browser_select_data_field(topo_container, 0)
+		for other_container in containers[number][1:]:
+			if other_container != None:
+				gwy_app_data_browser_merge(other_container)
+		gwy_file_save(topo_container, filebase + "/proc/scan" + str(number) + ".gwy", gwy.RUN_NONINTERACTIVE)
