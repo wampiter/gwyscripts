504d9e47fc93b2a576c02449e6abfff6f2004a16
Scott R. Johnston <wampiter@gmail.com>
2016-08-18 10:44:22 -0700

    changed directory

--- save_proc.py
+++ save_proc.py
@@ -8,8 +8,8 @@ for c in cons:
     filename = c.get_string_by_name("/filename")#get full filename
     filename = os.path.splitext(filename)[0]#Remove filename extension
     [path,name] = os.path.split(filename)#Splite filepath from filename
-    if not os.path.exists('%s/proc' % path):
-        os.makedirs('%s/proc' % path)
+    if not os.path.exists('%s/manproc' % path):
+        os.makedirs('%s/manproc' % path)
     print('Saving %s' % name)
     #I like this palette:
     #c.set_string_by_name("/0/base/palette", "Gwyddion.net")
@@ -20,5 +20,5 @@ for c in cons:
     #Save .gwy and .png in proc folder.
     #gwy_file_save(c, '%s/proc/%s.gwy' % (path,name), gwy.RUN_NONINTERACTIVE)
     #gwy_file_save(c, '%s/proc/%s.png' % (path,name), gwy.RUN_NONINTERACTIVE)
-    gwyutils.save_dfield_to_png(c, key, '%s/proc/%s.png' % (path,name), gwy.RUN_NONINTERACTIVE)
-    gwy_file_save(c, '%s/proc/%s.gwy' % (path,name), gwy.RUN_NONINTERACTIVE)
+    gwyutils.save_dfield_to_png(c, key, '%s/manproc/%s.png' % (path,name), gwy.RUN_NONINTERACTIVE)
+    gwy_file_save(c, '%s/manproc/%s.gwy' % (path,name), gwy.RUN_NONINTERACTIVE)
